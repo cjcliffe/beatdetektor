85a662048e4a906ddb40a511f9fa2ab7a7712c80
Charles J. Cliffe <cj@cubicproductions.com>
2012-08-29 17:55:04 -0400

    License change to MIT

--- js/beatdetektor.js
+++ js/beatdetektor.js
@@ -1,33 +1,33 @@
 /*
- *  BeatDetektor.js
+ * BeatDetektor.js
  *
- *  BeatDetektor - CubicFX Visualizer Beat Detection & Analysis Algorithm
- *  Javascript port by Charles J. Cliffe and Corban Brook
+ * BeatDetektor - CubicFX Visualizer Beat Detection & Analysis Algorithm
+ * Javascript port by Charles J. Cliffe and Corban Brook
+ *  
+ * Copyright (c) 2009 Charles J. Cliffe.
  *
- *  Created by Charles J. Cliffe on 09-11-30.
- *  Copyright 2009 Charles J. Cliffe. All rights reserved.
+ * BeatDetektor is distributed under the terms of the MIT License.
+ * http://opensource.org/licenses/MIT
  *
- *  BeatDetektor is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- *  Please note that only the Javascript version of BeatDetektor is licensed
- *  under the terms of LGPL version 3; ports of BeatDetektor or derivatives
- *  in other languages are licensed under the terms of GPL version 3.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- *  BeatDetektor is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *  Please contact cj@cubicproductions.com if you seek alternate
- *  licensing terms for your project.
- *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
  */
+
 
 
 /* 
